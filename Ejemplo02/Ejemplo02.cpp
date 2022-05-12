0cb19f7256ef44dda7d3ab226c9906707faa21bc
Cesar Ruben Reyes Anticona <N00213856@upn.pe>
2022-05-12 14:30:19 -0500

    Codigo Completo

--- Ejemplo02/Ejemplo02.cpp
+++ Ejemplo02/Ejemplo02.cpp
@@ -1,1 +1,27 @@
 //hola 2
+#include <iostream>
+using namespace std;
+
+struct Fecha {
+	int dia, mes, anio;
+};
+
+struct Persona {
+	char nombre[20];
+	int edad, peso;
+	Fecha fecha;
+}persona = { "Cindy",20,45,2,12,1997 };
+
+struct Empleado {
+	Persona persona;
+	int salario;
+}trabajador = { "Steven",19,60,22,7,1998,1000 };
+
+int main() {
+	cout << "Persona: " << persona.nombre;
+	cout << "\nEdad: " << persona.edad;
+	cout << "\nPeso: " << persona.peso;
+	cout << "\nDia nacimiento: " << persona.fecha.dia;
+	cout << "\nMes nacimiento: " << endl;
+	return 0;
+}
